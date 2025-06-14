481ac2b55f4112ed8c11df2cdaa04f578bfaa929
sharath-00 <sharathnandakumar2005@gmail.com>
2025-06-14 16:02:03 +0530

    Re-saved model with compatible numpy

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 pip install --upgrade pip setuptools wheel
 
 # Force reinstall numpy properly
-pip install --force-reinstall --no-cache-dir numpy==2.0.2
+pip install --force-reinstall --no-cache-dir numpy==1.24.3
 
 # Install the rest of the dependencies
 pip install -r requirements.txt
