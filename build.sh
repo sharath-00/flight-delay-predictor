bebac656d4df8bc0d5cd1644ec2b3fb0b7a7454c
sharath-00 <sharathnandakumar2005@gmail.com>
2025-06-14 14:37:30 +0530

    Add deployment-ready setup

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-pip install --upgrade pip
+
+# Upgrade pip and setuptools
+pip install --upgrade pip setuptools wheel
+
+# Force reinstall numpy properly
 pip install --force-reinstall --no-cache-dir numpy==1.24.3
+
+# Install the rest of the dependencies
 pip install -r requirements.txt
